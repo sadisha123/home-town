de74844d5915e317b332a69b26eac8ad21689a98
Sadisha Eranga <sadishaeranga19678@gmail.com>
2021-05-10 15:29:52 +0530

    my second commit

--- src/practise.java
+++ src/practise.java
@@ -1,5 +1,6 @@
 public class practise {
     public static void main(String[] args) {
         System.out.println("my hometown is tangalle");
+        System.out.println("my village is polommaruwa");
     }
 }
